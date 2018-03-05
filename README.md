12c190740a66417a264df7d4265912d1599f2ec6
zcallan <kaln.delbridge@hotmail.com>
2018-03-05 11:50:54 +1100

    added bacon ipsum text population

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # sketch-bacon-ipsum
 
+Credits to [Bacon Ipsum](https://baconipsum.com/json-api/) for the API.
+
+Since this plugin uses `async`/`await`, please make sure your version of Node is up to date.
+
+---
+
 _This plugin was created using `skpm`. For a detailed explanation on how things work, checkout the [skpm Readme](https://github.com/skpm/skpm/blob/master/README.md)._
 
 ## Usage
---
+++ src/populate-text.js
@@ -0,0 +1,20 @@
+export default async context => {
+  const selectedLayers = context.selection;
+  const selectedCount = selectedLayers.count();
+
+  /* Make sure there is at least 1 layer to fill. */
+  if ( !selectedCount ) {
+    context.document.showMessage( '❌ Cannot perform Bacon Ipsum - No layers selected!' );
+    log( 'Cannot perform Bacon Ipsum - No layers selected!' );
+    return false;
+  }
+
+  /* Fetch the 'Bacon ipsum' paragraph. */
+  const request = await fetch( `https://baconipsum.com/api/?type=all-meat&paras=${selectedCount}&start-with-lorem=0` );
+  const baconIpsum = await request.json();
+
+  /* Fill each layer with 'Bacon ipsum ...' */
+  selectedLayers.forEach(( layer, index ) => {
+    layer.stringValue = baconIpsum[index];
+  });
+}
